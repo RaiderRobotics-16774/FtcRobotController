5ab3cee2393684a3fabb41238b551637b276f271
RaiderRobotics-16774 <robotics@nortoncityschools.org>
2025-12-24 22:54:58 -0500

    Create HelloWorld.java
    Created first ever java class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World");
+    }
+// display your name in the loop
+    public void loop() {
+        telemetry.addData("Hello", "Mr. Brooke");
+    }
+}
