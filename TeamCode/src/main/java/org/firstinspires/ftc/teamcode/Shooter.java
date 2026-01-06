59b275ee678094466fbb714bd692f77f396f7c2b
RaiderRobotics-16774 <robotics@nortoncityschools.org>
2026-01-06 15:54:46 -0500

    1/6/26
    Update on 1/6/26

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Robot Shooting Code Java")
+public class Shooter extends LinearOpMode {
+}
